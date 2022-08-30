5fbca8c977313f1ee34bbc0ef789d03924317b1a
Azizi <munaemazizi@gmail.com>
2022-08-30 12:03:25 +0430

    add midified files

---
+++ myJavaScript.js
@@ -0,0 +1,70 @@
+
+
+
+// const FruitsMarket = ['Apple', 'Orange', 'Banana', 'Graps', 'Melone'];
+//                         0           1          2        3       4
+
+// document.write(FruitsMarket[2]);
+
+
+// Object:
+// const PersonsCharacters = {behavior='good Behavior', Education='Educated', Kindness='Kind', Calm='patient', understanding='Well Knowen, Degree='16-degree'};
+// document.write(PersonsCharacters.Degree);
+// document.write(PersonsCharacters.behavior);
+
+
+// for(let key in PersonsCharacters){
+    // this.document.write(key +"=>"+ personcharacters[key] +" ");
+// }
+
+
+
+
+
+// // Functions:
+
+// var breadPrices = 10;
+
+// var myBread;
+
+// var myChange;
+
+// function goGetBread(myBadge)
+// {
+//     myChange = Math.floor(myBadge%breadPrices);
+//     myBread = Math.floor(myBadge/breadPrices);
+//     document.write("number of Bread"+myBread);
+//     document.write("my Change money"+myChange);
+
+// }
+
+// goGetBread(209);
+
+
+
+// function of converting fahrenheit to Celsius:
+
+// function toCelsius(fahrenheit)
+// {
+//     var Cel  = ((5/9) * fahrenheit-32);
+//     return (5/9) * (fahrenheit-32);
+// }
+// document.getElementById('Demo').innerHTML="today the temprature is" + toCelsius;
+// document.write(toCelsius(98.6));
+
+
+// Displaying Hi and Hello Function>:
+
+// function sayHi()
+// {
+//     document.getElementById('Demo').innerHTML='Hi';
+// }
+// function sayHello()
+// {
+//     document.getElementById('Demo').innerHTML='Hello';
+// }
+
+
+
+
+
